16435dfee43505a43d24ddb4e54790e5e4f81e2f
HUAWEI <HUAWEI@LAPTOP-ODIBQ17D>
2020-07-02 15:31:16 +0800

    修改完成

--- test/src/test/FirstTest.java
+++ test/src/test/FirstTest.java
@@ -3,7 +3,7 @@ package test;
 public class FirstTest {
 
 	public static void main(String[] args) {
-		System.out.println("helloworlhhhd!!!11");
+		System.out.println("helloworlhhhd!!!!!11");
 
 	}
 
